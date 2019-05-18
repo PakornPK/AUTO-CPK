b6f35fa929efe29b1c6b78e4e57f840a153f55ea
PakornPK <sweetie_jimmie@hotmail.com>
2019-05-18 13:49:58 +0700

    :)

--- autocpk.cpp
+++ autocpk.cpp
@@ -9,9 +9,6 @@
 #include <cstdlib>
 #include <numeric>
 
-template <typename T, size_t N> const T* mybegin(const T (&a)[N]) { return a; }    
-template <typename T, size_t N> const T* myend  (const T (&a)[N]) { return a+N; }
-
 using namespace std;
 
 
