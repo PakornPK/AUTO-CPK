f7193307922c6d99b7c7f93e9873ebe6e96381ad
PakornPK <sweetie_jimmie@hotmail.com>
2019-05-23 08:39:17 +0700

    power vector

--- power_vactor.cpp
+++ power_vactor.cpp
@@ -2,19 +2,28 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <cmath>
 
 using namespace std; 
 
 int main(){ 
     
     vector<double> data = {1,2,3,4,5,6,7,8,9,10};
-    vector<double> data2 = data; 
-    transform(data2.begin()+1,data2.end(),data2.begin()+1,data2.end(),multiplies<double>());
+    vector<double> data2 ; 
+    double power; 
 
     vector<double>::iterator ptr; 
-    for (ptr =data2.begin(); ptr != data2.end(); ++ptr)
+    for (ptr =data.begin(); ptr != data.end(); ++ptr)
     {
-        cout << *ptr << endl;
+        power =0; 
+        power += pow(*ptr,2);
+        data2.push_back(power);
     }
+
+    for (ptr = data2.begin(); ptr != data2.end(); ++ptr)
+    {
+        cout << *ptr << ',';
+    }
+    
     return 0;
 }
